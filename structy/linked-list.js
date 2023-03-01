cee3806fcea59a4b4822ec111e1256d6670aed27
Linden Young <imlindenyoung@gmail.com>
2023-03-01 13:31:46 -0600

    structy section 2 - linked lists

--- structy/linked-list.js
+++ structy/linked-list.js
@@ -1,10 +1,54 @@
 // LL values
+const linkedListValues = (head) => {
+  const values = [];
+  let curr = head;
+
+  while (curr) {
+    values.push(curr.val);
+    curr = curr.next;
+  }
+
+  return values;
+};
 
 // sum list
+const sumList = (head) => {
+  let sum = 0;
+  let curr = head;
+
+  while (curr) {
+    sum += curr.val;
+    curr = curr.next;
+  }
+
+  return sum;
+};
 
 // LL find
+const linkedListFind = (head, target) => {
+  let curr = head;
+
+  while (curr) {
+    if (curr.val === target) return true;
+    curr = curr.next;
+  }
+
+  return false;
+};
 
 // get node value
+const getNodeValue = (head, index) => {
+  let curr = head;
+  let count = 0;
+
+  while (curr) {
+    if (count === index) return curr.val;
+    curr = curr.next;
+    count++;
+  }
+
+  return null;
+};
 
 // reverse list
 
