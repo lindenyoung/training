a9093f3cf305d6c06ad31da255a37f1d902281d6
Linden Young <imlindenyoung@gmail.com>
2023-03-02 11:14:56 -0600

    structy section 2 continued - linked lists

--- structy/linked-list.js
+++ structy/linked-list.js
@@ -51,14 +51,106 @@ const getNodeValue = (head, index) => {
 };
 
 // reverse list
+const reverseList = (head) => {
+  let curr = head;
+  let prev = null;
+
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
 
 // zipper lists
+const zipperLists = (head1, head2) => {
+  const head = head1;
+  let tail = head;
+  let curr1 = head1.next;
+  let curr2 = head2;
+  let count = 0;
+
+  while (curr1 && curr2) {
+    if (count % 2 === 0) {
+      tail.next = curr2;
+      curr2 = curr2.next;
+    } else {
+      tail.next = curr1;
+      curr1 = curr1.next;
+    }
+
+    tail = tail.next;
+    count++;
+  }
+
+  if (curr1) tail.next = curr1;
+  if (curr2) tail.next = curr2;
+
+  return head;
+};
 
 // merge lists
+const mergeLists = (head1, head2) => {
+  let dummyHead = new Node(null);
+  let tail = dummyHead;
+  let curr1 = head1;
+  let curr2 = head2;
+
+  while (curr1 && curr2) {
+    if (curr1.val < curr2.val) {
+      tail.next = curr1;
+      curr1 = curr1.next;
+    } else {
+      tail.next = curr2;
+      curr2 = curr2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  if (curr1) tail.next = curr1;
+  if (curr2) tail.next = curr2;
+
+  return dummyHead.next; // first node in new list
+};
 
 // is univalue list
+const isUnivalueList = (head) => {
+  let curr = head;
+
+  while (curr.next) {
+    if (curr.val !== curr.next.val) return false;
+    curr = curr.next;
+  }
+
+  return true;
+};
 
 // longest streak
+const longestStreak = (head) => {
+  if (!head) return 0; // edge case of null head node
+
+  let curr = head;
+  let streak = 1;
+  let maxStreak = 0;
+
+  while (curr.next) {
+    if (curr.next.val === curr.val) {
+      streak++;
+      curr = curr.next;
+    } else {
+      maxStreak = Math.max(maxStreak, streak);
+      streak = 1;
+      curr = curr.next;
+    }
+  }
+
+  maxStreak = Math.max(maxStreak, streak); // this catches case for when longest streak includes tail node, so line 143 never executes
+  return maxStreak;
+};
 
 // remove node
 
