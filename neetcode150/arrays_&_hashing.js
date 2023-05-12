092aa5ea5bec00044d99a11ce27b560e9bfe0943
lindenyoung <imlindenyoung@gmail.com>
2023-05-12 09:43:32 -0500

    complete neetcode daily problem

--- neetcode150/arrays_&_hashing.js
+++ neetcode150/arrays_&_hashing.js
@@ -1,4 +1,16 @@
 // Contains Duplicate
+const containsDuplicate = (nums) => {
+  // SET: O(n) time and O(n) space
+  const numsSet = new Set(nums);
+  return nums.length === numsSet.size;
+
+  // SORT: O(n * log(n)) time and O(1) space
+  // const sortedNums = nums.sort((a, b) => a - b);
+  // for (let i = 0; i < sortedNums.length; i++) {
+  //   if (sortedNums[i] === sortedNums[i + 1]) return true;
+  // }
+  // return false;
+}
 
 
 // Valid Anagram
