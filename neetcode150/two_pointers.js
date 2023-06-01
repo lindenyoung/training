c6667b2653f8df3605f1bd6bbe204713be489e41
lindenyoung <imlindenyoung@gmail.com>
2023-06-01 09:47:31 -0500

    neetcode daily problem - three sum

--- neetcode150/two_pointers.js
+++ neetcode150/two_pointers.js
@@ -37,6 +37,7 @@ const twoSumSorted = (nums, target) => {
 }
 
 // Container With Most Water (maxArea) - height: number[]
+// O(n) | O(1)
 const maxArea = (height) => {
   let [left, right, max] = [0, height.length - 1, 0];
 
@@ -52,4 +53,42 @@ const maxArea = (height) => {
   }
 
   return max;
+}
+
+// Three Sum
+// O(n^2) | O(1)
+const threeSum = (nums) => {
+  if (nums.length < 3) return []; // edge case of input array being too small
+
+  const result = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    const curr = nums[i];
+    const prev = nums[i - 1];
+    if (curr > 0) break; // impossible to sum to zero if first num is positive
+
+    if (i > 0 && curr === prev) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = curr + nums[left] + nums[right];
+
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else { // sum === 0
+        result.push([curr, nums[left], nums[right]]); // found a threeSum match
+        left++;
+        right--;
+        while (nums[left] === nums[left - 1] && left < right) left++; // skip left duplicates
+        while (nums[right] === nums[right + 1] && left < right) right--; // skip right duplicates
+      }
+    }
+  }
+
+  return result;
 }
