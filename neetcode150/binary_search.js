4a32489bab16ba4fb03887f08388bb30f5da57f3
lindenyoung <imlindenyoung@gmail.com>
2023-06-22 16:21:46 -0500

    neetcode daily problem - search 2d matrix

--- neetcode150/binary_search.js
+++ neetcode150/binary_search.js
@@ -1,6 +1,6 @@
 
 /**
- * Binary Search 704
+ * 704: Binary Search
  * nums = [-1, 0, 3, 5, 9, 12], target = 9 -> 4
  * solution must be O(log n) runtime complexity
  * @param {number[]} nums 
@@ -17,11 +17,35 @@ const search = (nums, target) => {
     
     // if mid num is target, return its index
     if (midNum === target) return midIndex
-    
+
     // if mid num is too small, set left to the index after mid index
     // otherwise mid num is too large, set right to index before mid index
     midNum < target ? left = midIndex + 1 : right = midIndex - 1
   }
 
   return -1
+}
+
+/**
+ * 74: Search a 2D Matrix
+ * matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 -> true
+ * @param {number[][]} matrix 
+ * @param {number} target 
+ * @return {boolean}
+ */
+const searchMatrix = (matrix, target) => {
+  const [rows, cols] = [matrix.length, matrix[0].length]
+  let [left, right] = [0, rows * cols - 1] // tread the matrix as a 1D array
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2) // (left + right) >> 1 is the bitwise shift version of this
+    const [row, col] = [Math.floor(mid / cols), mid % cols] // get 2D coordinates / indexes
+    const guess = matrix[row][col]
+
+    if (guess === target) return true
+    if (guess < target) left = mid + 1
+    if (guess > target) right = mid - 1
+  }
+  
+  return false
 }
