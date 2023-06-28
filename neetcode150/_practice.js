9d7f0b6a572f27f2884c1d2878f9d5b5b08b6c23
lindenyoung <imlindenyoung@gmail.com>
2023-06-28 16:15:39 -0500

    neetcode daily problem - search in rotated array

--- neetcode150/_practice.js
+++ neetcode150/_practice.js
@@ -162,3 +162,17 @@ const bananas = (piles, hours) => {
 
   return right
 }
+
+const minInRotated = (nums) => {
+  let left = 0,
+      right = nums.length - 1
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2)
+    // if mid num is > right num, can move our left pointer to right window
+    if (nums[mid] > nums[right]) left = mid + 1
+    else right = mid
+  }
+
+  return nums[left]
+}
--- neetcode150/binary_search.js
+++ neetcode150/binary_search.js
@@ -96,3 +96,34 @@ const findMin = (nums) => {
 
   return nums[left]
 }
+
+/**
+ * 33: Search in Rotated Sorted Array
+ * nums = [4,5,6,7,0,1,2], target = 0, returns 4
+ * @param {number[]} nums 
+ * @param {number} target 
+ * @return {number}
+ */
+const searchRotatedArr = (nums, target) => {
+  let left = 0,
+      right = nums.length - 1 
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+
+    if (nums[mid] === target) return mid
+
+    // find sorted half
+    if (nums[left] <= nums[mid]) {
+      // check if target is in sorted half (left) and adjust pointers
+      if (nums[left] <= target && target < nums[mid]) right = mid - 1
+      else left = mid + 1
+    } else {
+      // check if target is in sorted half (right) and adjust pointers
+      if (nums[mid] <= target && target <= nums[right]) left = mid + 1
+      else right = mid - 1
+    }
+  }
+
+  return -1
+}
