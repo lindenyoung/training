64573e5bfdfa7b4d05cbcea58cf2416d35379efd
lindenyoung <imlindenyoung@gmail.com>
2023-07-13 15:45:10 -0600

    neetcode daily problem - addTwoNumbers

--- neetcode150/linked_list.js
+++ neetcode150/linked_list.js
@@ -184,3 +184,18 @@ const copyRandomList = (head) => {
 
   return clones.get(head)
 }
+
+/**
+ * 2: Add Two Numbers
+ * https://leetcode.com/problems/add-two-numbers/solutions/3077723/only-4-lines-of-code-high-level-solution-explained-all-coding-steps/
+ * @param {ListNode} l1 
+ * @param {ListNode} l2 
+ * @return {ListNode}
+ */
+const addTwoNumbers = (l1, l2, carry = 0) => {
+  if (!l1 && !l2 && !carry) return null
+
+  const total = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + (carry || 0)
+  carry = parseInt(total / 10)
+  return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry))
+}
