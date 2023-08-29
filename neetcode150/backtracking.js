2b48c239891868c19d1b427e11be478ed1c365e2
lindenyoung <imlindenyoung@gmail.com>
2023-08-29 16:53:32 -0500

    letterCombinations

--- neetcode150/backtracking.js
+++ neetcode150/backtracking.js
@@ -236,3 +236,40 @@ const partition = (s) => {
     return true
   }
 }
+
+/**
+ * 17: Combinations of a Phone Number
+ * @param {string} digits
+ * @returns {string[]}
+ */
+const letterCombinations = (digits) => {
+  if (!digits.length) return []
+
+  const phoneMap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  }
+
+  const result = []
+
+  dfs(0, '')
+  return result
+
+  // helper recursive backtracking function
+  function dfs(i, str) {
+    if (i === digits.length) {
+      result.push(str)
+      return
+    }
+
+    for (const letter of phoneMap[digits[i]]) {
+      dfs(i + 1, str + letter)
+    }
+  }
+}
