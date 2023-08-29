4f8d268144a79cb6aee87c9fa524d894aa4ce9d6
lindenyoung <imlindenyoung@gmail.com>
2023-08-29 17:04:30 -0500

    numIslands

---
+++ neetcode150/graphs.js
@@ -0,0 +1,32 @@
+/**
+ * 200: Number of Islands
+ * @param {string[][]} grid 
+ * @returns {number}
+ */
+const numIslands = (grid) => {
+  const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+  let result = 0
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === '1') {
+        result++
+        traverse(row, col)
+      }
+    }
+  }
+
+  return result
+
+  function traverse(row, col) {
+    // return case - cell is out of bounds or water
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === '0') return
+
+    grid[row][col] = '0'
+
+    // recursive traversal in each direction
+    for (const [x, y] of directions) {
+      traverse(row + x, col + y)
+    }
+  }
+}
