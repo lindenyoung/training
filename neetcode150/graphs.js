d6306adde3021ae5725858445c40a142dac22464
lindenyoung <imlindenyoung@gmail.com>
2023-09-01 09:30:05 -0500

    maxAreaOfIsland

--- neetcode150/graphs.js
+++ neetcode150/graphs.js
@@ -30,3 +30,35 @@ const numIslands = (grid) => {
     }
   }
 }
+
+/**
+ * 695: Max Area of Island
+ * @param {number[][]} grid 
+ * @return {number}
+ */
+const maxAreaOfIsland = (grid) => {
+  const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+  let result = 0
+  
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === 1) result = Math.max(result, traverse(row, col))
+    }
+  }
+
+  return result
+
+  function traverse(row, col) {
+    // return case - cell is out of bounds or is a water cell
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] === 0) return 0
+
+    grid[row][col] = 0
+    let count = 1
+
+    for (const [x, y] of directions) {
+      count += traverse(row + x, col + y)
+    }
+
+    return count
+  }
+}
