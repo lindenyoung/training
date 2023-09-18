a4493448cc430efdbf3b8294e8476eda49415b6f
lindenyoung <imlindenyoung@gmail.com>
2023-09-18 16:47:54 -0500

    mergeIntervals and eraseOverlapIntervals

--- neetcode150/intervals.js
+++ neetcode150/intervals.js
@@ -49,3 +49,45 @@ const canAttendMeetings = (intervals) => {
 
   return true
 }
+
+/**
+ * 56: Merge Intervals
+ * @param {number[][]} intervals 
+ * @returns {number[][]}
+ */
+const mergeIntervals = (intervals) => {
+  if (!intervals.length) return []
+  intervals.sort((a, b) => a[0] - b[0])
+
+  const result = [intervals[0]]
+
+  for (const interval of intervals) {
+    const [start, end] = interval
+    const prevEnd = result[result.length - 1][1]
+
+    if (start <= prevEnd) result[result.length - 1][1] = Math.max(end, prevEnd)
+    else result.push(interval)
+  }
+
+  return result
+}
+
+/**
+ * 435: Non-overlapping Intervals
+ * @param {number[][]} intervals 
+ * @returns {number}
+ */
+const eraseOverlapIntervals = (intervals) => {
+  if (!intervals.length) return []
+  intervals.sort((a, b) => a[1] - b[1])
+
+  let result = 0,
+      prevEnd = intervals[0][1]
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] < prevEnd) result++
+    else prevEnd = intervals[i][1]
+  }
+
+  return result
+}
