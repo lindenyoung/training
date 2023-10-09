e1239af6ee6b8181b3e97c7b8364092a6b7c7f36
lindenyoung <imlindenyoung@gmail.com>
2023-10-09 17:14:49 -0500

    gtci fast and slow pointers

--- capital_one_prep/grokking_tci/fast_and_slow_pointers.js
+++ capital_one_prep/grokking_tci/fast_and_slow_pointers.js
@@ -87,3 +87,83 @@ const happyNumber = (num) => {
     return sum
   }
 }
+
+// Palindrome linked list - leetcode 234
+// Constraints:
+// - solution should be O(n) time and O(1) space
+const isPalindromeLL = (head) => {
+  if (!head || !head.next) return true
+  
+  let slow = head,
+      fast = head
+
+  // Floyd's algo to find the middle node
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  let prev = slow
+  prev.next = null
+  slow = slow.next
+
+  // reverse second half of ll
+  while (slow) {
+    const temp = slow.next
+    slow.next = prev
+    prev = slow
+    slow = temp
+  }
+
+  fast = head // first half head
+  slow = prev // second half reversed head
+
+  // compare first and second halves
+  while (slow) {
+    if (fast.val !== slow.val) return false
+    else {
+      slow = slow.next
+      fast = fast.next
+    }
+  }
+
+  return true
+}
+
+// Reorder linked list - leetcode 143
+// Do not return anything, modify input ll in-place and solution must be O(1) space
+const reorderList = (head) => {
+  // floyd's to find middle node
+  let slow = head,
+      fast = head
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  // reverse second half
+  let prev = null,
+      curr = slow.next
+
+  while (curr) {
+    const temp = curr.next
+    curr.next = prev
+    prev = curr
+    curr = temp
+  }
+
+  slow.next = null
+
+  // reorder the two halves
+  let h1 = head,
+      h2 = prev
+
+  while (h2) {
+    // creative way to rotate the merge from left to right halves
+    const temp = h1.next
+    h1.next = h2
+    h1 = h2
+    h2 = temp
+  }
+}
