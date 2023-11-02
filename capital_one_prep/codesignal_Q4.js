b4cceeff9ea437a1ca75450f1f39b67f9af41d41
lindenyoung <imlindenyoung@gmail.com>
2023-11-02 17:23:27 -0500

    codesignal Q4 practice

--- capital_one_prep/codesignal_Q4.js
+++ capital_one_prep/codesignal_Q4.js
@@ -119,7 +119,8 @@ const canReorderDoubled = (arr) => {
   }
 
   for (const num of arr) {
-    // need this bc we delete both num and pair props from map after finding pair
+    // need this bc we delete both num and pair from the map after finding a match
+    // so this condition skips nums that we have already matched on and deleted (-2 in the above example)
     if (!map[num]) continue
 
     // handle positive / negative nums differently
@@ -137,20 +138,104 @@ const canReorderDoubled = (arr) => {
 }
 
 /**
- * 3 sum with multiplicity
+ * 3 sum with multiplicity (combinatorics solution) - leetcode 923
+ * Input: arr = [1,1,2,2,3,3,4,4,5,5], target = 8
+ * Output: 20
+ * Explanation: Enumerating by the values (arr[i], arr[j], arr[k]): (1, 2, 5) occurs 8 times, (1, 3, 4) occurs 8 times, (2, 2, 4) occurs 2 times, (2, 3, 3) occurs 2 times.
+ * Constraints:
+* 3 <= arr.length <= 3000
+* 0 <= arr[i] <= 100
+* 0 <= target <= 300
  * @param {number[]} arr
  * @param {number} target 
  * @returns {number}
+ * O(n^2) time and O(n) space
  */
-const threeSumMulti = (arr, target) => {}
+// very fast solution but kinda of hard to understand
+const threeSumMulti = (arr, target) => {
+  let nmap = new Uint16Array(101),
+      third = target / 3,
+      ans = 0
+
+  for (let i in arr) nmap[arr[i]]++
+  
+  for (let k = Math.min(target, 100); k >= third; k--) {
+    let rem = target - k,
+        half = rem / 2
+    
+    for (let j = Math.min(rem, k); j >= half; j--) {
+        let i = rem - j,
+            x = nmap[i],
+            y = nmap[j],
+            z = nmap[k],
+            res
+        
+        if (i === k) res = x * (x-1) * (x-2) / 6
+        else if (i === j) res = x * (x-1) / 2 * z
+        else if (j === k) res = x * y * (y-1) / 2
+        else res = x * y * z
+        ans = (ans + res) % 1000000007
+    }
+  }
+
+  return ans
+}
+
+// much slower solution but easier to understand
+const threeSumMulti2 = (arr, target) => {
+  const map = {},
+        mod = 1000000007
+
+  let result = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    result = (result + (map[target - arr[i]] || 0)) % mod
+
+    for (let j = 0; j < i; j++) {
+      const temp = arr[i] + arr[j]
+      map[temp] = (map[temp] || 0) + 1
+    }
+  }
+
+  return result
+}
 
 /**
  * Count number of nice subarrays - leetcode 1248
+ * Input: nums = [1,1,2,1,1], k = 3
+ * Output: 2
+ * Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
  * @param {number[]} nums 
  * @param {number} k 
  * @returns {number}
  */
-const numberOfSubarrays = (nums, k) => {}
+const numberOfSubarrays = (nums, k) => {
+  const isOdd = (n) => n % 2 !== 0
+  
+  let odds = 0,
+      currCount = 0, // curr count of nice subarrays
+      left = 0,
+      result = 0
+
+  // expand window
+  for (let right = 0; right < nums.length; right++) {
+    if (isOdd(nums[right])) {
+      odds++
+      currCount = 0
+    }
+
+    // shrink window condition
+    while (odds === k && left <= right) {
+      if (isOdd(nums[left])) odds--
+      left++
+      currCount++
+    }
+
+    result += currCount
+  }
+
+  return result
+}
 
 /**
  * K-diff pairs in an array - leetcode 532
